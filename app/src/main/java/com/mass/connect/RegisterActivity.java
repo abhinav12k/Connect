f2653c1d978060b88b1ddf0a7d993f92db852428
Abhinav <abhi2000.rtk@gmail.com>
2020-04-12 01:36:56 +0530

    Added user database on firebase

--- app/src/main/java/com/mass/connect/RegisterActivity.java
+++ app/src/main/java/com/mass/connect/RegisterActivity.java
@@ -20,6 +20,10 @@ import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -29,6 +33,9 @@ public class RegisterActivity extends AppCompatActivity {
     private Button mCreateBtn;
     private FirebaseAuth mAuth;
     private Toolbar mToolbar;
+
+    private FirebaseDatabase database= FirebaseDatabase.getInstance();
+    private DatabaseReference mDatabaseReference;
 
     //progress bar
     private ProgressDialog mRegProgress;
@@ -64,36 +71,55 @@ public class RegisterActivity extends AppCompatActivity {
                 String email = mEmail.getEditText().getText().toString();
                 String password = mPassword.getEditText().getText().toString();
 
-            if(!TextUtils.isEmpty(displayName)||!TextUtils.isEmpty(email)||!TextUtils.isEmpty(password)){
+                if (!TextUtils.isEmpty(displayName) || !TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)) {
 
-                mRegProgress.setTitle("Registering User");
-                mRegProgress.setMessage("Please wait while we create your account !");
-                mRegProgress.setCanceledOnTouchOutside(false);
-                mRegProgress.show();
-                register_user(displayName, email, password);
+                    mRegProgress.setTitle("Registering User");
+                    mRegProgress.setMessage("Please wait while we create your account !");
+                    mRegProgress.setCanceledOnTouchOutside(false);
+                    mRegProgress.show();
+                    register_user(displayName, email, password);
 
-            }
+                }
 
             }
         });
 
     }
 
-    private void register_user(String displayName, String email, String password) {
-        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this,new OnCompleteListener<AuthResult>() {
+    private void register_user(final String displayName, String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
 //                Log.i("Before firebase R","ok");
-                if(task.isSuccessful()){
-                    mRegProgress.dismiss();
-                    Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
-                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(mainIntent);
-                    finish();
-                }else{
+                if (task.isSuccessful()) {
+
+                    FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                    String Uid = currentUser.getUid();
+
+                    mDatabaseReference = database.getReference().child("Users").child(Uid);
+                    HashMap<String, String> userMap = new HashMap<>();
+                    userMap.put("name", displayName);
+                    userMap.put("status", "Hi there I'm using Connect!");
+                    userMap.put("image", "default");
+                    userMap.put("thumb_image", "default");
+
+                    mDatabaseReference.setValue(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                mRegProgress.dismiss();
+                                Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                                mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(mainIntent);
+                                finish();
+                            }
+                        }
+                    });
+
+                } else {
                     mRegProgress.hide();
-                    Log.i("Error :",task.getException().toString());
-                    Toast.makeText(RegisterActivity.this,"Cannot Signin please check the details and try again.",Toast.LENGTH_SHORT).show();
+                    Log.i("Error :", task.getException().toString());
+                    Toast.makeText(RegisterActivity.this, "Cannot Signin please check the details and try again.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
