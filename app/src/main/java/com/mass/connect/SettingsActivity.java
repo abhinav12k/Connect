f4d84d4ed118f1ad9ac926a3448d1417f81cc700
Abhinav <abhi2000.rtk@gmail.com>
2020-04-19 04:24:53 +0530

    Fixed bugs

--- app/src/main/java/com/mass/connect/SettingsActivity.java
+++ app/src/main/java/com/mass/connect/SettingsActivity.java
@@ -4,15 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -24,6 +27,7 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
@@ -44,9 +48,13 @@ public class SettingsActivity extends AppCompatActivity {
     private CircleImageView mDisplayImage;
     private TextView mName;
     private TextView mStatus;
+    private ImageView profileImage;
 
     private Button changeImageBtn;
     private Button changeStatusBtn;
+
+    //progress dialog
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +66,7 @@ public class SettingsActivity extends AppCompatActivity {
         mStatus = findViewById(R.id.settings_status);
         changeImageBtn = findViewById(R.id.settings_image_btn);
         changeStatusBtn = findViewById(R.id.settings_status_btn);
+        profileImage = findViewById(R.id.settings_image);
 
         //setting up firebase
         mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
@@ -78,6 +87,7 @@ public class SettingsActivity extends AppCompatActivity {
 
                 mName.setText(name);
                 mStatus.setText(status);
+                Picasso.get().load(image).into(profileImage);
 
             }
 
@@ -109,7 +119,7 @@ public class SettingsActivity extends AppCompatActivity {
 
                 CropImage.activity()
                         .setGuidelines(CropImageView.Guidelines.ON)
-                        .setAspectRatio(1,1)
+                        .setAspectRatio(1, 1)
                         .start(SettingsActivity.this);
             }
         });
@@ -132,18 +142,46 @@ public class SettingsActivity extends AppCompatActivity {
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
+
+                mProgressDialog = new ProgressDialog(SettingsActivity.this);
+                mProgressDialog.setTitle("Saving image");
+                mProgressDialog.setMessage("Please wait while we update your profile photo");
+                mProgressDialog.show();
+
                 Uri resultUri = result.getUri();
 
-                String current_user_id = mFirebaseUser.getUid();
+                final String current_user_id = mFirebaseUser.getUid();
 
-                StorageReference storedImage = mImageRef.child("profile_images").child(current_user_id+".jpg");
-                storedImage.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                StorageReference file_path = mImageRef.child("profile_images").child(current_user_id + ".jpg");
+
+                file_path.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
                         if (task.isSuccessful()) {
-                            Toast.makeText(SettingsActivity.this, "Working", Toast.LENGTH_SHORT).show();
+
+                            mImageRef.child("profile_images").child(current_user_id + ".jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    String downloadUrl = uri.toString();
+                                    mDatabase.child("image").setValue(downloadUrl).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                mProgressDialog.dismiss();
+                                                Toast.makeText(SettingsActivity.this, "Profile Photo updated Successfully!", Toast.LENGTH_SHORT).show();
+                                            } else {
+                                                Toast.makeText(SettingsActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                                                mProgressDialog.dismiss();
+                                            }
+                                        }
+                                    });
+                                }
+                            });
+
                         } else {
                             Toast.makeText(SettingsActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                            mProgressDialog.dismiss();
                         }
                     }
                 });
@@ -154,17 +192,5 @@ public class SettingsActivity extends AppCompatActivity {
 
     }
 
-//    protected String getSaltString() {
-//        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-//        StringBuilder salt = new StringBuilder();
-//        Random rnd = new Random();
-//        while (salt.length() < 18) { // length of the random string.
-//            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
-//            salt.append(SALTCHARS.charAt(index));
-//        }
-//        String saltStr = salt.toString();
-//        return saltStr;
-//
-//    }
 
 }
