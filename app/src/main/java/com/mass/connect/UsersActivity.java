77ca04d08024251c803acbf2a7aaa5c910cad59b
Abhinav <abhi2000.rtk@gmail.com>
2020-05-12 18:31:44 +0530

    Added all users activity

---
+++ app/src/main/java/com/mass/connect/UsersActivity.java
@@ -0,0 +1,103 @@
+package com.mass.connect;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class UsersActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private RecyclerView mRecyclerView;
+    private DatabaseReference mUsersDatabase;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users);
+
+        mUsersDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        mToolbar = findViewById(R.id.users_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("All Users");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Users> options = new FirebaseRecyclerOptions.Builder<Users>()
+                .setQuery(mUsersDatabase,Users.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Users,UsersViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Users, UsersViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull UsersViewHolder holder, int position, @NonNull Users users) {
+                holder.setName(users.getName());
+                holder.setStatus(users.getStatus());
+            }
+
+            @NonNull
+            @Override
+            public UsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_user_activity,parent,false);
+                return new UsersViewHolder(view);
+            }
+        };
+
+        mRecyclerView.setAdapter(firebaseRecyclerAdapter);
+        firebaseRecyclerAdapter.startListening();
+    }
+
+    public static class UsersViewHolder extends RecyclerView.ViewHolder{
+
+        private View mView;
+        private TextView user_name;
+        private TextView user_status;
+        private CircleImageView user_profilePic;
+
+        public UsersViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setName(String name){
+            user_name = mView.findViewById(R.id.mProfileName);
+            user_name.setText(name);
+        }
+
+        public void setStatus(String status){
+            user_status = mView.findViewById(R.id.default_status);
+            user_status.setText(status);
+        }
+
+        public void setProfilePic(String profilePic){
+            user_profilePic = mView.findViewById(R.id.mProfileImage);
+//            user_profilePic.setImageBitmap();
+        }
+    }
+
+}
