0db48056c588e517e01ff27cee14008506e70a7a
Abhinav <abhi2000.rtk@gmail.com>
2020-05-13 01:25:45 +0530

    Added friend request feature

--- app/src/main/java/com/mass/connect/ProfileActivity.java
+++ app/src/main/java/com/mass/connect/ProfileActivity.java
@@ -1,22 +1,128 @@
 package com.mass.connect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private TextView mDisplayID;
+    private ImageView mProfileImage;
+    private TextView mProfileName;
+    private TextView mProfileStatus;
+    private TextView mProfileFriendsCount;
+    private Button mProfileSendReqBtn;
+    private Button mProfileDeclineReqBtn;
+
+    private DatabaseReference mUsersDatabase;
+    private DatabaseReference mFriendReqDatabase;
+
+    private ProgressDialog mProgressDialog;
+
+    private String mCurrent_state;
+
+    private FirebaseUser mCurrent_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        String user_id = getIntent().getStringExtra("user_id");
+        final String user_id = getIntent().getStringExtra("user_id");
 
-        mDisplayID = findViewById(R.id.profile_Display_Name);
-        mDisplayID.setText(user_id);
+        mUsersDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
+        mFriendReqDatabase = FirebaseDatabase.getInstance().getReference().child("Friend_req");
+        mCurrent_user = FirebaseAuth.getInstance().getCurrentUser();
+
+        mProfileName = findViewById(R.id.profile_Display_Name);
+        mProfileImage = findViewById(R.id.profileImage);
+        mProfileStatus = findViewById(R.id.profile_status);
+        mProfileFriendsCount = findViewById(R.id.profile_total_friends);
+        mProfileSendReqBtn = findViewById(R.id.profile_sendFriendRequest);
+        mProfileDeclineReqBtn = findViewById(R.id.profile_declineFriendRequest);
+
+        mCurrent_state = "not_friends";
+
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setTitle("Loading....");
+        mProgressDialog.setMessage("Please wait while we load the data.");
+        mProgressDialog.setCanceledOnTouchOutside(false);
+        mProgressDialog.show();
+
+        mUsersDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String display_name = dataSnapshot.child("name").getValue().toString();
+                String status = dataSnapshot.child("status").getValue().toString();
+                String image = dataSnapshot.child("image").getValue().toString();
+
+                mProfileName.setText(display_name);
+                mProfileStatus.setText(status);
+
+                Picasso.get().load(image).placeholder(R.drawable.default_avatar).into(mProfileImage);
+
+                mProgressDialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+        mProfileSendReqBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(mCurrent_state.equals("not_friends")){
+
+                    mFriendReqDatabase.child(mCurrent_user.getUid()).child(user_id).child("request_type").setValue("sent")
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            
+                            if(task.isSuccessful()) {
+                                mFriendReqDatabase.child(user_id).child(mCurrent_user.getUid()).child("request_type").setValue("received")
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Toast.makeText(ProfileActivity.this, "Request Sent Successfully!", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                            }else{
+                                Toast.makeText(ProfileActivity.this, "Unable to send request", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                }
+
+            }
+        });
+
+
     }
+
+
 }
